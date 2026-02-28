012418b8c91864ee36627fcd0f6793c66786f9dd
kalim <kalim.ahmetchin2003@gmail.com>
2026-02-28 20:31:29 +0300

    init

---
+++ src/debug.h
@@ -0,0 +1,9 @@
+#ifndef INTERPRETER_DEBUG_H
+#define INTERPRETER_DEBUG_H
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif //INTERPRETER_DEBUG_H
