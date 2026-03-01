d8f4b7fe4fbfb603f89016731d52f2d84b8dd635
kalim <kalim.ahmetchin2003@gmail.com>
2026-03-01 13:15:58 +0300

    virtual machine

--- src/main.c
+++ src/main.c
@@ -1,18 +1,38 @@
+#include <stdio.h>
+
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
+    // пример выражения: -((1.2 + 3.4) / 5.6)
     int constant = addConstant(&chunk, 1.2);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
+    constant = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+
+    writeChunk(&chunk, OP_NEGATE, 123);
     writeChunk(&chunk, OP_RETURN, 123);
 
-    disassembleChunk(&chunk, "test chunk");
-    freeChunk(&chunk);
+    interpret(&chunk);
 
+    freeVM();
+    freeChunk(&chunk);
     return 0;
 }
---
+++ src/vm.h
@@ -0,0 +1,28 @@
+#ifndef INTERPRETER_VM_H
+#define INTERPRETER_VM_H
+
+#include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk* chunk;
+    uint8_t* ip;
+    Value stack[STACK_MAX]; // память для массива выделяется автоматически
+    Value* stackTop;
+} VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
+
+void initVM();
+void freeVM();
+InterpretResult interpret(Chunk* chunk);
+void push(Value value);
+Value pop();
+
+#endif //INTERPRETER_VM_H
