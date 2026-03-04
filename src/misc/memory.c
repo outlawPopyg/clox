32290f900ce1ccd6d86866dd3d826564ef2b50f8
kalim <kalim.ahmetchin2003@gmail.com>
2026-03-04 18:35:56 +0300

    strings

--- src/misc/memory.c
+++ src/misc/memory.c
@@ -2,6 +2,12 @@
 
 #include "memory.h"
 
+#include "object.h"
+#include "value.h"
+#include "../vm/vm.h"
+
+#define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
+
 void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
     if (newSize == 0) {
         free(pointer);
@@ -11,4 +17,23 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
     void* result = realloc(pointer, newSize);
     if (result == NULL) exit(1); // кончилась память
     return result;
+}
+
+static void freeObject(Obj* object) {
+    switch (object->type) {
+        case OBJ_STRING: {
+            ObjString* string = (ObjString*) object;
+            FREE_ARRAY(char, string->chars, string->length + 1);
+            FREE(ObjString, object);
+        }
+    }
+}
+
+void freeObjects() {
+    Obj* object = vm.objects;
+    while (object != NULL) {
+        Obj* next = object->next;
+        freeObject(object);
+        object = next;
+    }
 }
